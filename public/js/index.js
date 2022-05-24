8515e3543408e8ffaa4b9a2697f45afa592c0860
Alex Racciatti <racciattialex@gmail.com>
2022-05-24 12:27:27 -0300

    Feat: Movies searcher by ID and by Rating now working

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () =>{
         //Movies buttons
         const allMovies = document.getElementById('allMovies');
         const oneMovieForm = document.getElementById('oneMovieForm');
-        const movieRating = document.getElementById('movieRating');
+        const movieRating = document.getElementById('movieRatingForm');
     
         //Actors buttons
         const allActors = document.getElementById('allActors');
@@ -16,17 +16,17 @@ window.addEventListener('load', () =>{
     
         //Info container
         const infoContainer = document.getElementById('info-container');
-    
-        //Table body
+        const reusableContainer = document.getElementById('reusable-container')
+
+        //Table selectors head and body
+        const tableStructure = document.getElementById('table')
         const tableBody = document.getElementById('table-body');
+
+        //Loader
+        const loader = document.getElementById('loader');
     
     
     function getMoviesFromApi() {
-
-        let movies = [];
-
-        if(movies == ""){
-        };
 
         fetch('https://moviesbackapi.herokuapp.com/api/movies')
         .then(function(res){
@@ -34,16 +34,15 @@ window.addEventListener('load', () =>{
         })
         .then(function(data){
 
-            // infoContainer.innerHTML = "";
-
-
-            movies = data.data;
+            let movies = data.data;
             console.log(movies);
 
             infoContainer.classList.remove('visually-hidden');
+            tableStructure.classList.remove('visually-hidden');
             infoContainer.classList.add('movies-container');
-           
-            
+            infoContainer.classList.remove('genres-container');
+            infoContainer.classList.remove('actors-container');
+
             movies.forEach(movie => {
                 tableBody.innerHTML += `
                 <tr>
@@ -51,10 +50,11 @@ window.addEventListener('load', () =>{
                     <td>${movie.title}</td>
                     <td>${movie.genre.name}</td>
                     <td>${movie.rating}</td>
-                    <td>${movie.length}</td>
                 </tr>
                 `
             });
+
+            loader.classList.add('visually-hidden');
         })
     };
     
@@ -62,30 +62,124 @@ window.addEventListener('load', () =>{
         fetch(`https://moviesbackapi.herokuapp.com/api/movies/${id}`)
         .then(r => r.json())
         .then(data => {
-            console.log(data.data.title);
+            let movie = data.data;
+
+            //Adding styles
+            infoContainer.classList.add('movies-container');
+            tableStructure.classList.add('visually-hidden');
+
+            //Deleting styles
+            infoContainer.classList.remove('visually-hidden');
+            infoContainer.classList.remove('actors-container');
+            infoContainer.classList.remove('genres-container');
+
+            //Show movie on DOM
+            infoContainer.innerHTML += `
+                <section id="reusable-container" class="container-sm text-center my-2 py-3 border border-2 border-primary rounded">
+                    <img src="../public/img/img-undefined.png" height="150px" class="rounded mx-auto d-block">
+                    <h4 class="py-2">${movie.title}</h4>
+                    <ul class="list-group">
+                        <li class="list-group-item">ID N° ${movie.id}</li>
+                        <li class="list-group-item">Duración: ${movie.length} minutos</li>
+                        <li class="list-group-item">Género: ${movie.genre_id}) ${movie.genre.name}</li>
+                        <li class="list-group-item">Rating: ${movie.rating}</li>
+                        <li class="list-group-item">Premios: ${movie.awards}</li>
+                    </ul>
+                </section>
+            `
+
+            //Hide loader
+            loader.classList.add('visually-hidden');
         })
     };
     
     function getMovieFromApiByMinRating(rating) {
-        
+        fetch(`https://moviesbackapi.herokuapp.com/api/movies/recomended/${rating}`)
+          .then(r=>r.json())
+          .then(data=>{
+              console.log(data.data);
+
+              let moviesByRating = data.data;
+
+              infoContainer.classList.remove('visually-hidden');
+              tableStructure.classList.remove('visually-hidden');
+              infoContainer.classList.add('movies-container');
+              infoContainer.classList.remove('genres-container');
+              infoContainer.classList.remove('actors-container');
+  
+              moviesByRating.forEach(movie => {
+                  tableBody.innerHTML += `
+                  <tr>
+                      <th scope='row'>${movie.id}</th>
+                      <td>${movie.title}</td>
+                      <td>${movie.genre.name}</td>
+                      <td>${movie.rating}</td>
+                  </tr>
+                  `
+                });
+
+              loader.classList.add('visually-hidden');
+          })
     };
 
     
 
+    allMovies.addEventListener('click', () => {
+        loader.classList.remove('visually-hidden');
+        getMoviesFromApi()
+
+        allMovies.classList.add('disabled');
+    })
+
     oneMovieForm.addEventListener('submit', event => {
         event.preventDefault();
+        loader.classList.remove('visually-hidden');
+
+        //Capture the ID from the form
         let movieId = event.target.movieId.value;
-        if (movieId == '') return alert("¡Debes ingresar un id!");
+
+        //Convert the ID to a number type.
+        parseInt(movieId);
+
+        //Catch possible User mistakes
+        if (movieId == ''){
+            loader.classList.add('visually-hidden');
+            return alert("¡Debes ingresar un id!");
+        } 
+            
+        if (movieId > 21){
+            loader.classList.add('visually-hidden');
+            return alert("¡El número que introduzcas debe corresponder a alguna película! Te recomendamos revisar el listado de películas.")
+        } 
+        
+        //Call the method to do the fetch and show the correct movie
         getMovieFromApiById(movieId);
 
     });
 
-    allMovies.addEventListener('click', () => {
-        getMoviesFromApi();
+    movieRating.addEventListener('submit', event =>{
+        event.preventDefault();
+        loader.classList.remove('visually-hidden');
+
+        //Capture the ID from the form
+        let movieRating = event.target.movieRating.value;
+
+        //Convert the ID to a number type.
+        parseInt(movieRating);
+
+        //Catch possible User mistakes
+        if (movieRating == '') {
+            loader.classList.add('visually-hidden');
+            return alert("¡Debes ingresar un rating válido!");
+        }
         
+        if (movieRating > 10) {
+            loader.classList.add('visually-hidden');
+            return alert("¡El rating que introduzcas debe estar entre 1.0 y 10.0!")
+        }
+        //Call the method to do the fetch and show the correct movie
+        getMovieFromApiByMinRating(movieRating);
     })
-
-
 
     
 
